b1c69d582d2e27eddc956b71b3842da5d7c42036
Daevah Taylaran <daevtayl@gmail.com>
2022-05-16 20:54:26 -0700

    added remove function

--- script.js
+++ script.js
@@ -1,54 +1,3 @@
-// let button = document.getElementById("enter");
-// let input = document.getElementById("userinput");
-// let lists = document.querySelectorAll("li");
-// let ul = document.querySelector("ul");
-
-// console.log(ul.children);
-
-// function inputLength() {
-//   return input.value.length;
-// }
-
-// function listCheck() {
-//   let userListInput = input.value;
-//   let groceryList = ul.children;
-
-//   for (let i = 0; i < groceryList.length; i++) {
-//     console.log(groceryList[i].innerHTML);
-//     if (groceryList[i].innerHTML.toLowerCase() === userListInput.toLowerCase()) {
-//       alert("Item is already on the list.");
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-// function createListElement() {
-//   let li = document.createElement("li");
-//   li.appendChild(document.createTextNode(input.value));
-//   ul.appendChild(li);
-//   input.value = "";
-// }
-
-// function addListAfterClick() {
-//   if (inputLength() > 0 && listCheck() === true) {
-//     createListElement();
-//     console.log(lists);
-//   }
-// }
-
-// function addListAfterKeyPress(event) {
-//   if (inputLength() > 0 && event.keyCode === 13 && listCheck() === true) {
-//     createListElement();
-//     console.log(lists);
-//   }
-// }
-
-// button.addEventListener("click", addListAfterClick);
-
-// input.addEventListener("keypress", addListAfterKeyPress);
-
-
 //second version
 
 let button = document.getElementById("enter");
@@ -69,7 +18,6 @@ let lastListItem = grabList[listLength - 1];
 function listCheck() {
   for (let key of listItemDiv) {
     let listItem = key.children[0].innerHTML;
-
 
     if (input.value.toLowerCase() === listItem.toLowerCase() || 
     input.value.toLowerCase() === lastListItem.innerHTML.toLowerCase() ||
@@ -111,3 +59,41 @@ function noRepeat() {
 }
 
 button.addEventListener("click", noRepeat);
+button.addEventListener("click", removeDiv);
+
+removeDiv();
+
+console.log(ul.children);
+
+
+// function removeDiv() {
+//   for (let key of ul.children) {
+//     let listButton = key.children[1];
+//     console.log(listButton);
+
+//     listButton.addEventListener("click", remove);
+
+//     function remove() {
+//       key.remove();
+//     }
+//   }
+// }
+
+function removeDiv() {
+  console.log(ul.children);
+  for (let key of ul.children) {
+    console.log(ul.children);
+    let divElement = document.querySelectorAll('.list-item');
+    for (let key of divElement) {
+      let listButton = key.children[1];
+      listButton.addEventListener("click", remove);
+
+      function remove() {
+        key.remove();
+      }
+    }
+  }
+}
+
+
+
